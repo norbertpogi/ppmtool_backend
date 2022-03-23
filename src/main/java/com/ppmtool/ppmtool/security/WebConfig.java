873ba6e13c2df776672413f6510da8d00e5858c7
Norbert Bautista <norbert.bautista@ibm.com>
2022-03-23 09:32:11 +0800

    update corss

--- src/main/java/com/ppmtool/ppmtool/security/WebConfig.java
+++ src/main/java/com/ppmtool/ppmtool/security/WebConfig.java
@@ -11,7 +11,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+
     }
 
 }
